b962c395f33b1976679b529182ab09fdadc52649
asajenko <asajenko@gmail.com>
2020-03-27 21:30:42 +0100

    add junits libs and create tests for saveKudosPeriod

---
+++ src/test/java/com/asajenko/kudos/KudosAppTest.java
@@ -0,0 +1,12 @@
+package com.asajenko.kudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KudosAppTest {
+
+    @Test
+    public void loadContext() {
+    }
+}
